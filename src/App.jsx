cea5b3190695238e1101aab1943dd334a2054089
fiamanillah <fiamanillahb2@gmail.com>
2024-11-03 20:53:08 +0600

    Assignment start with the json data

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+	const [productData, setProductData] = useState([]);
+
+	useEffect(() => {
+		async function dataFetch() {
+			const response = await fetch("gadget_heaven_products.json");
+			const data = await response.json();
+			setProductData(data);
+		}
+		dataFetch();
+	}, []);
+
+	return (
+		<div>
+			<ul>
+				{productData.map((prod) => (
+					<li className="p-3" key={prod.product_id}>
+						<span>{prod.product_title}</span>
+            <img src={prod.product_image} alt="" />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default App;
