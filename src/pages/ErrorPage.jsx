095f8bf32a764b95368da44bc10aebfa1055d161
fiamanillah <fiamanillahb2@gmail.com>
2024-11-06 03:35:52 +0600

    Small changes

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function ErrorPage() {
+
+	const location = useLocation();
+
+	useEffect(() => {
+		document.title = "404 Not Found";
+	}, [location]);
+
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+		<div className="flex flex-col items-center justify-start h-screen">
+			<img className="max-w-[300px]" src="/404.png" alt="" />
 			<p className="text-lg mb-6">Sorry, the page you are looking for does not exist.</p>
 			<Link to="/" className="text-primary underline">
 				Go back to Home
